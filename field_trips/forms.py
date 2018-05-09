dae0edc7596bfd2f2497ff3ea7bb1016841d5724
Ryan Tolboom <Ryan.Tolboom@monroe.k12.nj.us>
2018-05-08 21:17:27 -0400

    Cleaning up / modularizing create view

--- field_trips/forms.py
+++ field_trips/forms.py
@@ -2,12 +2,11 @@ from django import forms
 
 from .models import FieldTrip, Chaperone
 
-class CreateForm(forms.ModelForm):
+class FieldTripForm(forms.ModelForm):
 
     class Meta:
         model = FieldTrip
-        exclude = ['email', 'transported_by', 'transportation_comments',
-            'nurse_required', 'nurse_comments', 'nurse_name']
+        fields = [] # default to nothing showing
         widgets = {
                 'destination': forms.TextInput(attrs={'class': 'form-control'}),
                 'group': forms.TextInput(attrs={'class': 'form-control'}),
@@ -21,12 +20,23 @@ class CreateForm(forms.ModelForm):
                 'directions': forms.FileInput(attrs={'class': 'form-control'}),
                 'buses': forms.NumberInput(attrs={'class': 'form-control'}),
                 'extra_vehicles': forms.SelectMultiple(attrs={'class': 'form-control'}),
+                'transported_by': forms.TextInput(attrs={'class': 'form-control'}),
+                'transportation_comments': forms.Textarea(attrs={'class': 'form-control'}),
                 'costs': forms.Textarea(attrs={'class': 'form-control'}),
                 'funds': forms.Select(attrs={'class': 'form-control'}),
                 'anticipatory': forms.Textarea(attrs={'class': 'form-control'}),
                 'purpose': forms.Textarea(attrs={'class': 'form-control'}),
                 'standards': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
+# This is the form someone sees when they create a new field trip request
+class CreateForm(FieldTripForm):
+
+    class Meta(FieldTripForm.Meta):
+        fields = ['destination', 'group', 'grades', 'roster', 'itinerary',
+            'pupils', 'teachers', 'departing', 'returning', 'directions',
+            'buses', 'extra_vehicles', 'costs', 'funds', 'anticipatory',
+            'purpose', 'standards']
 
 class ChaperoneForm(forms.ModelForm):
 
