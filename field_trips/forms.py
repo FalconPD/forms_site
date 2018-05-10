3ce9572c32cc0ada6e857d0139525a246c5228db
Ryan Tolboom <Ryan.Tolboom@monroe.k12.nj.us>
2018-05-09 20:31:56 -0400

    Fixed DateTime format issues

--- field_trips/forms.py
+++ field_trips/forms.py
@@ -2,15 +2,24 @@ from django import forms
 
 from .models import FieldTrip, Chaperone, Approver
 
+DATETIME_FORMAT = "%m/%d/%Y %I:%M %p"
+
 class FieldTripForm(forms.ModelForm):
+
+    # Our date format is different, so we have to override these to specify it
+    departing = forms.DateTimeField(
+        widget=forms.DateInput(format=DATETIME_FORMAT),
+        input_formats=(DATETIME_FORMAT,)
+    )
+    returning = forms.DateTimeField(
+        widget=forms.DateInput(format=DATETIME_FORMAT),
+        input_formats=(DATETIME_FORMAT,)
+    )
 
     class Meta:
         model = FieldTrip
         fields = [] # default to nothing showing
-        widgets = {
-            'departing': forms.DateTimeInput(attrs={'class': 'datetimepicker'}),
-            'returning': forms.DateTimeInput(attrs={'class': 'datetimepicker'}),
-        }
+
 
 # This is the form someone sees when they create a new field trip request
 class CreateForm(FieldTripForm):
