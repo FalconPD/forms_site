262d7d4293e74990cff74a90ab1588b6b831cbee
Ryan Tolboom <Ryan.Tolboom@Monroe.K12.NJ.US>
2018-05-14 12:32:39 -0400

    Reset DB, added fixtures, working on views

--- field_trips/urls.py
+++ field_trips/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'field_trips'
 urlpatterns = [
     path('create', views.create, name='create'),
+    path('<int:pk>/', views.detail, name='detail'),
 ]
--- field_trips/views.py
+++ field_trips/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.forms import formset_factory
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 
 from .forms import CreateForm, ChaperoneForm
+from .models import FieldTrip
 
 @login_required
 def create(request):
@@ -19,3 +20,9 @@ def create(request):
         formset = ChaperoneFormFactory()
         form = CreateForm()
     return render(request, 'field_trips/create.html', {'form': form, 'formset': formset})
+
+@login_required
+def detail(request, pk):
+    field_trip = get_object_or_404(FieldTrip, pk=pk)
+    return render(request,'field_trips/details.html', {'field_trip': field_trip,
+        'fields': ('destination', 'departing')})
