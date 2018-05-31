f5d1c1f8a93d6a753863a4a11ee0f54a75c26a4d
Ryan Tolboom <Ryan.Tolboom@Monroe.K12.NJ.US>
2018-05-31 19:47:32 -0400

    Heroku integration

--- forms_site/settings.py
+++ forms_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -148,3 +149,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+django_heroku.settings(locals())
